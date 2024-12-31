4feea833b5c70e06663b91766e440fd0ce482447
andreybrumatti <andreyporto004@gmail.com>
2024-12-31 10:55:21 -0300

    Adicionando novos componentes e funcionalidades

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,19 @@
 import { useEffect, useState } from "react";
-import api from '../../services/api';
+import api from '../../services/api'; //Saindo da pasta Home, e pages, para acessar a pasta services
+import './home.css'
+import { Link } from "react-router-dom";
 
 // URL da api: /movie/now_playing?api_key=e68807db0339972b24cea8042fb9741d&language=pt-BR
 
 function Home(){
 
     const [filmes, setFilmes] = useState([]);
+    const [loading, setLoadind] = useState(true);
 
 
     useEffect(() =>{
-        async function loadFilmes() {
-            const response = await api.get("movie/now_playing", {
+        async function loadFilmes() { // async: Transforma a função em assíncrona ou seja leva um tempo para ir na api, e buscar a lista de filme
+            const response = await api.get("movie/now_playing", { //await: função vai aguardar a requisição ser feita para prosseguir ou entao devolver algum erro
                 params: {
                     api_key: "e68807db0339972b24cea8042fb9741d",
                     language: "pt-BR",
@@ -18,7 +21,10 @@ function Home(){
                 }
             })
 
-            console.log(response.data.results);
+            //console.log(response.data.results.slice(0, 10)); //Pega apenas os 10 primeiros filmes da lista [ 0 á 9]
+
+            setFilmes(response.data.results.slice(0, 10));
+            setLoadind(false);
 
         }
 
@@ -26,9 +32,30 @@ function Home(){
 
     }, []);
 
+
+    if(loading){ //se a página estiver carregando, executa o código
+        return(
+            <div className="loading">
+                <h2>Carregando filmes...</h2>
+            </div>
+        )
+    }
+
     return(
-        <div>
-            <h1>Home</h1>
+        <div className="container">
+            
+            <div className="lista-filmes">
+                {filmes.map((filme) =>{ //Map passará por todos os elementos dos filmes, retornando apenas o que pedir no article
+                    return(
+                       <article key={filme.id}>
+                        <strong>{filme.title}</strong>
+                        <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+                        <Link to={`/filme/${filme.id}`}>Acessar</Link> 
+                       </article> 
+                    )
+                })}
+            </div>
+            
         </div>
     )
 }
--- src/services/api.js
+++ src/services/api.js
@@ -4,8 +4,8 @@ import axios from 'axios';
 // https://api.themoviedb.org/3/movie/now_playing?api_key=e68807db0339972b24cea8042fb9741d&language=pt-BR
 
 
-const api = axios.create({
-    baseURL: 'https://api.themoviedb.org/3/'
+const api = axios.create({ //Criando uma instância personalizada do Axios
+    baseURL: 'https://api.themoviedb.org/3/' //Define a URL base para todas as requisições que serão feitas usando essa instância do Axios.
 });
 
 export default api;
